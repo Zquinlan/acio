11ba2a95d0192fe3f5881eedee6ac0a6c709e408
Zach Quinlan <Zquinlan@gmail.com>
2021-05-03 11:56:22 -0700

    resource path for build button

--- src/acio_gui.py
+++ src/acio_gui.py
@@ -361,6 +361,11 @@ class mainWindow(QMainWindow):
         self.createMenuBar()
         self.initUI()
 
+    def resource_path(relative_path):
+        if hasattr(sys, '_MEIPASS'):
+            return os.path.join(sys._MEIPASS, relative_path)
+         return os.path.join(os.path.abspath("."), relative_path)
+
     def createMenuBar(self):
         # Making the Menu bar
         mainMenu = self.menuBar()
@@ -504,7 +509,7 @@ class mainWindow(QMainWindow):
         layout.addWidget(self.socialButton)
 
         # Make website button
-        acioIcon = QIcon('Acio_design_v0.01.png')
+        acioIcon = QIcon(resource_path('Acio_design_v0.01.png'))
         self.mkFramework = QPushButton("Acio Website", self)
         self.mkFramework.setIcon(acioIcon)
         self.mkFramework.setIconSize(QSize(30,30))
