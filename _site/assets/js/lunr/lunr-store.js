b9812988d85c69818a313d5253f970dc331a7abe
Zach Quinlan <Zquinlan@gmail.com>
2021-05-04 10:17:59 -0700

    update site

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,28 @@ var store = [{
         "tags": [],
         "url": "/about/",
         "teaser": null
+      },{
+        "title": "Contact us!",
+        "excerpt":"Constributors Zach Quinlan NSF GRFP scholar – 3rd year PhD student Scripps Institution of Oceanography Coral reef biogeochemist and data scientist. I am pursuing my PhD to help save coral reefs but am also fascinated by data in general and more so how we can increase the accessibility of our...","categories": [],
+        "tags": [],
+        "url": "/contact/",
+        "teaser": null
+      },{
+        "title": "Issues",
+        "excerpt":"Reporting: Before sending in issues please check below for known issues! During BETA please report any issue you have directly to me. In the future I will transition over to the github issues page. Known Issues: Git clone works exceptionally slowly on Windows with specific graphics card. Especially if it...","categories": [],
+        "tags": [],
+        "url": "/issues/",
+        "teaser": null
+      },{
+        "title": "Tutorial",
+        "excerpt":"Mac tutorial  Windows tutorial  ","categories": [],
+        "tags": [],
+        "url": "/tutorial/",
+        "teaser": null
+      },{
+        "title": "Update",
+        "excerpt":"Most recent updates: Release of AcIo open Beta! Future additions to be made to AcIO: Gihub Github sign in Github total integration into GUI Speeding up windows github GUI Edit AcIO generated website option Add page option Add posts option Update image folder option Version 2 Options for SEO so...","categories": [],
+        "tags": [],
+        "url": "/update/",
+        "teaser": null
       },]
