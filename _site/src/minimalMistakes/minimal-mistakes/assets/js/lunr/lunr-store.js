0bb543c3f0c30c2e14a29485b5e6e47ff99bf3e1
Zach Quinlan <Zquinlan@gmail.com>
2021-08-15 18:15:09 -1000

    updating website with correct download link

---
+++ _site/src/minimalMistakes/minimal-mistakes/assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "About",
+        "excerpt":"What is AcIO? AcIo is a static website generator for github repositories. It was designed for scientists so that we could better present our research and importantly code to a wider audience than just the group of researchers who are comforatble with coding and github enough to view it. The...","categories": [],
+        "tags": [],
+        "url": "/about/",
+        "teaser": null
+      },{
+        "title": "Contact us!",
+        "excerpt":"Constributors Zach Quinlan NSF GRFP scholar – 3rd year PhD student Scripps Institution of Oceanography Coral reef biogeochemist and data scientist. I am pursuing my PhD to help save coral reefs but am also fascinated by data in general and more so how we can increase the accessibility of our...","categories": [],
+        "tags": [],
+        "url": "/contact/",
+        "teaser": null
+      },{
+        "title": "Download",
+        "excerpt":"Most recent version:   beta v0.01: Download     release notes:     Release of AcIO Beta for Mac and Windows!        Prevoious Versions:  ","categories": [],
+        "tags": [],
+        "url": "/download/",
+        "teaser": null
+      },{
+        "title": "Issues",
+        "excerpt":"Reporting: Before sending in issues please check below for known issues! During BETA please report any issue you have directly to me. In the future I will transition over to the github issues page. Known Issues: Git clone works exceptionally slowly on Windows with specific graphics card. Especially if it...","categories": [],
+        "tags": [],
+        "url": "/issues/",
+        "teaser": null
+      },{
+        "title": "Tutorial",
+        "excerpt":"Before starting If you would like to help beta test AcIo, please email Zach to recieve the application executables. If you are running AcIo on windows you will have to download Git for windows if you have not previously done so. You will also have to setup your login credentials...","categories": [],
+        "tags": [],
+        "url": "/tutorial/",
+        "teaser": null
+      },{
+        "title": "Update",
+        "excerpt":"Most recent updates: Release of AcIo open Beta! Future additions to be made to AcIO: Gihub Github sign in Github total integration into GUI Speeding up windows github GUI Edit AcIO generated website option README as optional argument Add page option Add posts option Update image folder option Version 2...","categories": [],
+        "tags": [],
+        "url": "/update/",
+        "teaser": null
+      },]
